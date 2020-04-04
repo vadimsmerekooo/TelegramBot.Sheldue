47e497798091a48a38bc940fad970e2dd362ba19
Vadim <vadimsmerekooo@users.noreply.github.com>
2020-04-04 23:27:03 +0300

    New interface. Change Page for main grid. Change adaptive trigger's and fix bugs.

--- WindowAppMain/MainWindow.xaml.cs
+++ WindowAppMain/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media.Animation;
+using System.Windows.Navigation;
 using WindowAppMain.Model.Style.WindowStyle;
 
 namespace WindowAppMain
@@ -10,39 +13,94 @@ namespace WindowAppMain
     /// </summary>
     public partial class MainWindow : Window
     {
-
-
+        #region Parametr's
+        private bool StateClosed = true;
         string _name;
+        #endregion
         public MainWindow()
         {
             InitializeComponent();
             HomeBox.IsSelected = true;
+            
         }
         public MainWindow(string name): this()
         {
             _name = name;
             UserName.Text = _name;
         }
-        bool StateClosed = true;
+        //DoubleAnimation Open and Close UserPageInfo
         private void ButtonMenu_Click(object sender, RoutedEventArgs e)
         {
             if (StateClosed)
             {
                 Storyboard sb = this.FindResource("OpenMenu") as Storyboard;
                 sb.Begin();
+                MainWindowName.MinWidth = 710;
             }
             else
             {
                 Storyboard sb = this.FindResource("CloseMenu") as Storyboard;
                 sb.Begin();
+                MainWindowName.MinWidth = 500;
             }
 
             StateClosed = !StateClosed;
         }
+        //!DoubleAnimation Open and Close UserPageInfo
 
-        private void CloseButtonHamburgerMenu_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        //Select Page
+        private void GroupListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (GroupListBox.SelectedIndex)
+            {
+                case 0:
+                    MainWindowPage.NavigationService.Navigate(new Uri("Model/Window/MainWindowPage/HomePage.xaml", UriKind.Relative));
+                    break;
+                case 1:
+                    MainWindowPage.NavigationService.Navigate(new Uri("Model/Window/MainWindowPage/AccountInfoPage.xaml", UriKind.Relative));
+                    break;
+                case 2:
+                    MainWindowPage.NavigationService.Navigate(new Uri("Model/Window/MainWindowPage/SettingsPage.xaml", UriKind.Relative));
+                    break;
+            }
+        }
+        //!Select Page
+
+        //Event's TitleBar Change
+        private void ButtonFechar_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void WindowMinimized_Click(object sender, RoutedEventArgs e) { this.WindowState = WindowState.Minimized; }
+        private void WindowMaxNormal_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+                WindowMaxNormal.Kind = MaterialDesignThemes.Wpf.PackIconKind.WindowMaximize;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+                WindowMaxNormal.Kind = MaterialDesignThemes.Wpf.PackIconKind.WindowRestore;
+            }
+        }
+        private void GridTitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+        private void GridTitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(this.WindowState == WindowState.Maximized)
+                this.WindowState = WindowState.Normal;
+        }
+        //!Event's TitleBar Change
+
+        //Event's UserInfoPage
+        private void CloseAccount_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             this.Close();
         }
+        //!Event's UserInfoPage
     }
 }
